1ad89741d70dcea3be2cf77b5f7caa9e448f9283
kinshieslayer <kinkinshie@gmail.com>
2024-11-29 12:42:25 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,13 @@
+const { sayHello } = require('./app');
+
+function testSayHello() {
+  const result = sayHello();
+  if (result === 'Hello, World!') {
+    console.log('Test Passed: sayHello() = Hello, World!');
+  } else {
+    console.error('Test Failed: sayHello() =', result);
+  }
+}
+
+
+testSayHello();
